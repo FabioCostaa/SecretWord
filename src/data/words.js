21e33c9b47992760008ae2e753c886739dc2bbc7
FabioCostaa <fabio.glz4@gmail.com>
2023-03-18 20:49:20 -0300

    secretword

---
+++ src/data/words.js
@@ -0,0 +1,9 @@
+export const wordsList = {
+  carro: ["Motor", "Porta", "Capô", "Pneu","Escapamento","Roda", "Antena","Farol","Retrovisor"],
+  fruta: ["Banana", "Maçã", "Pêra", "Mamão","Melancia","Morango","Laranja","Abacate","Abacaxi"],
+  corpo: ["Braço", "Perna", "Cérebro", "Pescoço","Ossos","Cabeça", "Olhos","Coraçâo","Joelho"],
+  computador: ["Mouse", "Teclado","Processador","SSD", "Monitor", "Gabinete","MousePad","HeadSet","PlacaDeVideo"],
+  programação: ["Linguagem","Python", "Framework", "JavaScript", "React","Ruby","Java","HTML","CSS"],
+  alimento: ["Arroz", "Pizza","Hamburguer", "Feijão", "Carne", "Leite", "Ovo","Lasanha","Bolo"],
+  objeto: ["Copo","Ventilador","Lapis","Sapato","Caderno","Espelho","Cadeira","Caneta","Faca"]
+};
